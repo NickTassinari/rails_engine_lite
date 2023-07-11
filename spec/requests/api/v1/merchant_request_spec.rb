270c6cbd34a40880bcc6f0babd26b5e74247600c
Nick Tassinari <ntass7800@gmail.com>
2023-07-11 10:52:15 -0600

    add merchant show action and adequate testing

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -2,15 +2,35 @@ require 'rails_helper'
 
 describe "Merchant API" do 
   it "sends list of merchants" do 
-    create_list(:merchant, 3)
+    merchant = Merchant.create(name: "Target")
+    merchant2 = Merchant.create(name: "AMCH")
 
     get '/api/v1/merchants'
     
     expect(response).to be_successful 
 
     merchants = JSON.parse(response.body, symbolize_names: true)
-# require 'pry'; binding.pry
-    expect(merchants[:data].count).to eq(3)
 
+    expect(merchants[:data].count).to eq(2)
+    expect(merchants[:data][0][:attributes][:name]).to be_a(String)
+    expect(merchants[:data][0][:attributes][:name]).to eq("Target")
+    expect(merchants[:data][1][:attributes][:name]).to eq("AMCH")
+  end
+
+  it "can send single merchant data" do 
+    merchant = Merchant.create(name: "Target")
+
+    get "/api/v1/merchants/#{merchant.id}"
+    expect(response).to be_successful 
+    
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    # require 'pry'; binding.pry
+
+    expect(merchants[:data][:attributes][:name]).to be_a(String)
+    expect(merchants[:data][:attributes][:name]).to eq("Target")
+
+
+
+    
   end
 end
